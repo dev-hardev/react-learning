449f745115b3ff9a7a7b9c0f02a86c62a7e367a4
Hardev Singh <hardev@frappedev.co.in>
2022-02-24 20:10:24 +0530

    working on things

---
+++ src/crud/Index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Listing from "./Listing";
+function Index() {
+    return <Listing />;
+}
+
+export default Index;
---
+++ src/crud/Listing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Listing() {
+    return "Listing";
+}
+
+export default Listing;
